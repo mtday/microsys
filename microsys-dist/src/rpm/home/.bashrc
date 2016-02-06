26ede99d03384c1797805bd6bea7f27c9d5257e0
mtday <mday@42six.com>
2016-02-06 01:14:24 -0600

    added vagrant vm for testing, more rpm code

--- microsys-dist/src/rpm/home/.bashrc
+++ microsys-dist/src/rpm/home/.bashrc
@@ -4,3 +4,6 @@ if [[ -f /etc/bashrc ]]; then
     source /etc/bashrc
 fi
 
+# Add ${project.groupId} scripts to the path.
+export PATH="${PATH}:/opt/${project.groupId}/current/bin"
+
--- microsys-dist/src/rpm/scripts/postinstall.sh
+++ microsys-dist/src/rpm/scripts/postinstall.sh
@@ -4,6 +4,18 @@
 # The RPM post-install script.
 #
 
-# TODO: Need to update the /opt/${project.groupId}/current symlink to point to the new version.
 
+# Create a symlink for current to the newly installed version.
+ln -sf /opt/${project.groupId}/${project.version} /opt/${project.groupId}/current
+
+# Create a symlink for the /etc/init.d service script.
+ln -sf /opt/${project.groupId}/current/bin/service.sh /etc/init.d/${project.groupId}
+
+
+# Symlink the logs directory.
+if [[ ! -d /var/log/${project.groupId} ]]; then
+    mkdir /var/log/${project.groupId}
+    chown ${project.user}:${project.group} /var/log/${project.groupId}
+fi
+ln -sf /var/log/${project.groupId} /opt/${project.groupId}/current/logs
 
