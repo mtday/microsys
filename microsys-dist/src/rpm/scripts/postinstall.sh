0179afbb08f852d9fabc805d73c8b2cfbc200171
mtday <mday@42six.com>
2016-03-06 00:27:54 -0600

    updated configuration

--- microsys-dist/src/rpm/scripts/postinstall.sh
+++ microsys-dist/src/rpm/scripts/postinstall.sh
@@ -20,3 +20,18 @@ ln -sf /home/${project.groupId} /opt/${project.groupId}/current/home
 # Symlink the logs directory.
 ln -sf /var/log/${project.groupId} /opt/${project.groupId}/current/logs
 
+# Create the keystore and truststore if they do not exist.
+KEYSTORE="/home/${project.groupId}/pki/keystore.jks"
+TRUSTSTORE="/home/${project.groupId}/pki/truststore.jks"
+if [[ ! -f ${KEYSTORE} ]]; then
+    keytool -genkey -alias localhost -keyalg RSA -keystore ${KEYSTORE} -storepass changeit \
+        -dname "CN=localhost" -keypass changeit
+    chown ${project.groupId}:${project.groupId} ${KEYSTORE}
+    chmod 640 ${KEYSTORE}
+fi
+if [[ ! -f ${TRUSTSTORE} ]]; then
+    cp ${KEYSTORE} ${TRUSTSTORE}
+    chown ${project.groupId}:${project.groupId} ${TRUSTSTORE}
+    chmod 640 ${TRUSTSTORE}
+fi
+
